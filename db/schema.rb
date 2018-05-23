29c2eec1e27c6e54ba651ca447ec5e8328d01b98
ms2sato <ms2.sato@gmail.com>
2018-05-23 11:19:05 +0000

    add email to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_04_03_115850) do
+ActiveRecord::Schema.define(version: 2018_05_23_111540) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
+    t.string "email"
   end
 
 end
